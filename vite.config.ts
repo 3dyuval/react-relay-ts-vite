b1d80b261c40a95b4ebf0019e889c4c7d451222b
3dyuval <3dyuval@gmail.com>
2023-04-29 12:18:49 +0300

    vite-relay integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import * as path from 'node:path'
-
+import relay from 'vite-plugin-relay'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), relay],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, './src')
