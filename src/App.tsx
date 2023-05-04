acf960e23cdccde419b0c3e1c4abd18fec663bc9
3dyuval <3dyuval@gmail.com>
2023-05-04 09:48:21 +0300

    testing the environment

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from '@/assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { graphql, useLazyLoadQuery } from 'react-relay'
+import { AppFeedQuery } from './__generated__/AppFeedQuery.graphql'
+
+const feedQuery = graphql`
+  query AppFeedQuery {
+    feed {
+      id
+      description
+      url
+      comments {
+        id
+        body
+      }
+    }
+  }
+`
 
 function App() {
-  const [count, setCount] = useState(0)
+  const data = useLazyLoadQuery<AppFeedQuery>(feedQuery, {})
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>Hello relay</h1>
+        {data && data.feed.map(item => <div className="card" key={item.id}>
+          <span>{item.description}</span><span>{item.url}</span>
+        </div>)}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 import * as path from 'node:path'
 import relay from 'vite-plugin-relay'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), relay],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, './src')
     }
+  },
+  server: {
+    port: 3030
   }
 })
